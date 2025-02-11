1254e2db6445de7827d3cebe952b63c8712e663e
unknown <olhadub08@gmail.com>
2025-02-11 21:44:30 +0200

    Create realization for methods of UserPlate class and fix mistake in Ball class

--- Views/UserPlate.py
+++ Views/UserPlate.py
@@ -1,15 +1,28 @@
+import pygame
 from Views.Abstract_classes.AbstractMovableObject import AbstractMovableObject
-
+from Views.Scene import SceneObject
 class UserPlateObject(AbstractMovableObject):
 
-    def __init__(self, x_position: float, y_position: float, width: float, height: float, color: str):
+    def __init__(self, x_position: float, y_position: float, width: float, height: float, color: str, speed: float):
+        scene=SceneObject()
         super().__init__(x_position, y_position, height, width, color, True)
-
-    def render(self):
-        pass
+        self.scene_width =scene.width
+        self.speed = speed
+        self.rect=pygame.Rect(x_position, y_position, width, height)
+    def render(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
 
     def move_to(self, x: float, y: float):
-        pass
+        if 0<=x<self.scene_width - self.rect.width:
+            self.rect.x=x
+        else:
+            self.rect.x = max(0, min(x, self.scene_width - self.rect.width))
+        self.rect.y=y
 
     def resize(self, new_width: float):
-        pass
+        self.rect.width=new_width
+        if self.rect.right>self.scene_width:
+            self.rect.x=self.scene_width-self.rect.width
+
+
+
