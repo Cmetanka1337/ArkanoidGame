cb9587b9ff560417d1992f656fa5ba851c311f42
Vova Sribnyi <vovasribnyy22@gmail.com>
2025-02-12 11:57:14 +0200

    Changed argument color of type str to pygame.Color

--- Views/Abstract_classes/AbstractMovableObject.py
+++ Views/Abstract_classes/AbstractMovableObject.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+
+import pygame
+
 from Views.Abstract_classes.AbstractObject import AbstractObject
 
 '''
@@ -7,7 +10,7 @@ AbstractStaticObject is an abstract class that represents abstract some movable 
 class AbstractMovableObject(AbstractObject):
 
     @abstractmethod
-    def __init__(self, x_position: float, y_position: float, height: float, width: float, color: str, is_visible=True):
+    def __init__(self, x_position: float, y_position: float, height: float, width: float, color: pygame.Color, is_visible=True):
         super().__init__(x_position, y_position, height, width, color, is_visible)
 
     def move_to(self, x:float, y:float):
