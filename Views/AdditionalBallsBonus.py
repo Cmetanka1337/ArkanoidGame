98c742919585935a8af7bce5bbe9e9f14f695e76
Vova Sribnyi <vovasribnyy22@gmail.com>
2025-02-25 21:55:42 +0200

    Implemented Additional Balls Bonus

---
+++ Views/AdditionalBallsBonus.py
@@ -0,0 +1,28 @@
+from Views.Abstract_classes import AbstractObject
+from Views.Abstract_classes.AbstractBonusObject import AbstractBonusObject
+import pygame
+
+from Views.Ball import BallObject
+
+
+'''
+AdditionalBallsBonus its implementation of AbstractBonusObject for additional balls bonus
+'''
+class AdditionalBallsBonus(AbstractBonusObject):
+    balls_number:float
+    def __init__(self, x_position: float, y_position: float,
+                 height: float, width: float,
+                 color: pygame.Color, speed: float, move_direction: list, radius: float,
+                 is_active: bool, balls_number: float):
+        super().__init__(x_position, y_position, height, width, color, True,speed,move_direction,radius,is_active)
+        self.balls_number = balls_number
+    #adds balls to screen and activates bonus
+    def activate(self, target: "GameScreen"):
+        for ball in range(self.balls_number):
+            target.balls.append(BallObject(200*0.5*ball, 100*0.5*ball, 10, 10, pygame.Color(255, 0, 0), 5, [1, 1], 5,True))
+        self.is_active = True
+
+
+    def deactivate(self, target: AbstractObject):
+        pass
+
