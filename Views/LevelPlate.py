aa8961d11dee615bb56ef7aa485dc62ac6d4965b
Vova Sribnyi <vovasribnyy22@gmail.com>
2025-02-26 14:13:03 +0200

    Inserted Olia's implementation of level plates

--- Views/LevelPlate.py
+++ Views/LevelPlate.py
@@ -1,25 +1,43 @@
+import pygame
 from Models.Bonus import BonusObject
 from Views.Abstract_classes.AbstractStaticObject import AbstractStaticObject
 
 class LevelPlateObject(AbstractStaticObject):
-
+    alpha_values = {3: 255, 2: 170, 1: 85}
     hit_points: int
     plate_type: str # standard plate or with a bonus, for example
-
+    alpha:int
     def __init__(self,hit_points: int, plate_type: str, is_breakable: bool, x_position: float, y_position: float, height: float, width: float,
-                 color: str, is_visible=True):
+                 color: pygame.Color, is_visible=True):
         super().__init__(is_breakable, x_position, y_position, height, width, color, is_visible)
         self.hit_points = hit_points
         self.plate_type = plate_type
-
-    def render(self):
-        pass
+        self.alpha = 255
+        self.rect = pygame.Rect(x_position, y_position, width, height)
+        self.color.a = self.alpha
+    def render(self, screen):
+        """ Малює платформу, якщо вона видима """
+        if self.is_visible:
+            surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
+            surface.fill((self.color.r, self.color.g, self.color.b, self.alpha))
+            screen.blit(surface, self.rect.topleft)
 
     def decrease_hit_points(self):
-        pass
+        """ Зменшує міцність платформи, робить її невидимою при руйнуванні """
+        if self.is_breakable:
+            self.hit_points -= 1
+            if self.hit_points > 0:
+                self.alpha = self.alpha_values.get(self.hit_points,50)
+                self.color.a = self.alpha
+            else:
+                self.is_visible = False
+
 
     def spawn_bonus(self) -> BonusObject:
-        pass
+        if self.plate_type == 'bonus':
+            return BonusObject("d", True,5)
 
     def update_state(self):
-        pass
+        """ Плавне зникнення після руйнування"""
+        if not self.is_visible and self.alpha > 0:
+            self.alpha = max(self.alpha-10,0)
